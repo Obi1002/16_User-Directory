cee3a63745a72c1a68fa2e9f57749216984b5d72
Obi <obiude1002@gmail.com>
2021-06-07 21:51:03 -0400

    setting up gh pages

---
+++ precache-manifest.06ea5bd9ff5f3a8cc7e952896ebeb584.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f1f78ff2c584754cf34f9a82a630d0",
+    "url": "/16_User-Directory/index.html"
+  },
+  {
+    "revision": "c078dd34a17e4e4fd595",
+    "url": "/16_User-Directory/static/css/main.420d6436.chunk.css"
+  },
+  {
+    "revision": "08d8025173769a2fb7cb",
+    "url": "/16_User-Directory/static/js/2.b4c4101d.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/16_User-Directory/static/js/2.b4c4101d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd61c826a0e838e7ddb",
+    "url": "/16_User-Directory/static/js/3.90f17c04.chunk.js"
+  },
+  {
+    "revision": "fd77f13855b5676a8e117c5b6583996c",
+    "url": "/16_User-Directory/static/js/3.90f17c04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ac924e15b499bfc01a",
+    "url": "/16_User-Directory/static/js/4.bac5788b.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/16_User-Directory/static/js/4.bac5788b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551efece036bd485b5a8",
+    "url": "/16_User-Directory/static/js/5.d0e467f0.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/16_User-Directory/static/js/5.d0e467f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c078dd34a17e4e4fd595",
+    "url": "/16_User-Directory/static/js/main.c84d64b4.chunk.js"
+  },
+  {
+    "revision": "0c9ef525a2855ca2677a",
+    "url": "/16_User-Directory/static/js/runtime-main.d4d59401.js"
+  }
+]);
--- src/utils/API.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export default {
-    getEmployees: () => axios.get("https://dummy.restapiexample.com/api/v1/employees")
-};
